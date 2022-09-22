f7e24fe17b015475f2bd1ac26ed89447399625b1
Stormtyler <stormtyler1812@icloud.com>
2022-09-22 15:58:42 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print if positive, negative or zero
+ *
+ * Return: 0 
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a')
+		{
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
