4e2569623ccf05531e350381e2f8ff35b862deeb
Stormtyler <stormtyler1812@icloud.com>
2022-09-22 15:35:00 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print if positive or negative or zero
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
