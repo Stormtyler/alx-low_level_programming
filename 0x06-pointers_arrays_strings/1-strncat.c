b917c345b938731e10c05feb5a75b944b0fbf2ad
Stormtyler <stormtyler1812@icloud.com>
2022-09-22 17:51:43 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,12 +5,13 @@
  * _strncat -> function to append some charx
  * @dest: First Parameter
  * @src: Second Parameter
- * 2N: Third Paramter
+ * @n: Third Paramter
  * Return: String
  */
 
-char*_strncat(char*dest,char*src, in n)
+char *_strncat(char *dest, char *src, int n)
 {
 	strncat(dest, src, n);
 	return (dest);
 }
+
