cc34bc57fc12fe4fc62d4e5281bf03cfb2933430
Stormtyler <stormtyler1812@icloud.com>
2022-09-22 18:04:05 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp -> purposes to compare
+ * @s1: first parameter
+ * @s2: second parameter
+ * Return: integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
+
