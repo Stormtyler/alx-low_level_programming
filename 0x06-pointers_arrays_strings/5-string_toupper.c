c2fd2f540de135cbbd294836905b09049e79c30f
Stormtyler <stormtyler1812@icloud.com>
2022-09-22 18:13:42 -0700

    string to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper -> convert any string to upper
+ * @x: string parameter
+ * Return: string
+ */
+
+char *string_toupper(char *x)
+{
+	int i = 0;
+
+	while (x[i])
+	{
+		if (x[i] >= 97 && x[i] <= 122)
+			x[i] = x[i] - 32;
+		i++;
+	}
+	return (x);
+}
+
