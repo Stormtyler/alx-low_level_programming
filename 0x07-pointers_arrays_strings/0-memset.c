aadaa96dd288c985140d1fc1556f988d7da827b6
Stormtyler <stormtyler1812@icloud.com>
2022-09-26 13:28:14 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * _memset - a function that fills memory with a constant byte
  *
@@ -14,7 +16,6 @@
 char *_memset(char *s, char *b, unsigned int n)
 {
 	unsigned int i;
-
 	i = 0;
 
 	while (i < n)
