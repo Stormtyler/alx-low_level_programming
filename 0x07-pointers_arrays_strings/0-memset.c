8386e433c7e689a43d7b6a57ce92f5f4f36ade11
Stormtyler <stormtyler1812@icloud.com>
2022-09-26 13:32:08 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,8 +5,10 @@
  *
  * @s: input pointer to char type
  *    represents the pointer to the block of memory to fill
+ * 
  * @b: input variable of char type
  *    represents the character to fill s
+ * 
  * @n: unsigned int variable
  *    the number of bytes to be filled
  *
