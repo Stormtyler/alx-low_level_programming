85b7ed36f7d87fae823e46c56f068d6118cd9297
Stormtyler <stormtyler1812@icloud.com>
2022-09-26 13:43:55 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,9 @@
 #include "main.h"
 /**
  * _memset - a function that fills memory with a constant byte
- * @s: input pointer to char type
- *    represents the pointer to the block of memory to fill
- * @b: input variable of char type
- *    represents the character to fill s
- * @n: unsigned int variable
- *    the number of bytes to be filled
+ * @s: input pointer to char type represents the pointer to the block of memory to fill
+ * @b: input variable of char type represents the character to fill s
+ * @n: unsigned int variable the number of bytes to be filled
  *
  * Return: a pointer to the filled memory area @s
  */
