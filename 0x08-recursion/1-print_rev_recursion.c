8648fec0ec384f0d5fadb502aab3f941f8dc6b21
Stormtyler <stormtyler1812@icloud.com>
2022-11-02 12:28:36 +0200

    gitfix

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,19 +1,19 @@
-/*                                                                                                                                  
+/*
  * File: 1-print_rev_recursion.c
- * Auth: sam                                                        
- */                                                                                                          
-                                                                                                                                        
+ * Auth: sam
+ */
+
 #include "main.h"
 
 /**
- * _print_rev_recursion - Prints a string in reverse.
- * @s: The string to be printed.                                                                                                        
+ * _print_rev_recursions - Prints a string in reverse.
+ * @s: The string to be printed.
  */
 void _print_rev_recursion(char *s)
-{                                                                                                      
-        if (*s)
-        {
-                _print_rev_recursion(s + 1);
-                _putchar(*s);
-        }
+{
+	if(*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
